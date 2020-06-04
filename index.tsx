0ee24aa70c3102b9d1cda0f02e01489999ffa5fc
bailycase <bailycase99@gmail.com>
2020-06-04 13:29:43 -0500

    fix: fixed ip for ws & nginx.conf

--- index.tsx
+++ index.tsx
@@ -50,12 +50,12 @@ const Theme = deepMerge({
 
 // Create an http link:
 const httpLink = new HttpLink({
-  uri: 'https://10.15.248.36:3000/graphql',
+  uri: 'https://10.15.252.61:3000/graphql',
 });
 
 // Create a WebSocket link:
 const wsLink = new WebSocketLink({
-  uri: 'wss://10.15.248.36:3000/graphql',
+  uri: 'wss://10.15.252.61:3000/graphql',
   options: {
     reconnect: true,
   },
