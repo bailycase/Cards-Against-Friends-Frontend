9d6fe75676a94fb3cc1072cbedbbef6adb2dadd7
bailycase <bailycase99@gmail.com>
2020-06-09 21:41:06 -0500

    chore: fix workflow

--- .releaserc.js
+++ .releaserc.js
@@ -4,6 +4,12 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     ['@semantic-release/npm', { npmPublish: false }],
-    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
   ],
 };
