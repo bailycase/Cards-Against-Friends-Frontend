67022672d00f5067648967b9574350d04fa1b787
bailycase <bailycase99@gmail.com>
2020-06-09 21:50:19 -0500

    fix: fixed release pipeline

--- .releaserc.js
+++ .releaserc.js
@@ -5,7 +5,7 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     ['@semantic-release/npm', { npmPublish: false }],
     [
-      '@semantic-release/github',
+      '@semantic-release/git',
       {
         assets: ['package.json'],
         message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
